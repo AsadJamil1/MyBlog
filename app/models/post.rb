cc6496b46177deef92d2728d87e07ba4e8922556
AsadJamil <asadmengal158@gmail.com>
2017-05-14 01:12:51 +0500

    added associations between models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
-	has_many :comments, dependent: :destroy
+	has_many :user 
+	has_many :comments, dependent: :destroy, :through => :user
+
 	validates :title, presence: true, length: {minimum: 5}
 	validates :body, presence: true
 end
