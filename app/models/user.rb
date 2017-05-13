398d0d77e50abb271b0707b7b674226e6d98d82a
AsadJamil <asadmengal158@gmail.com>
2017-05-14 02:07:53 +0500

    Complete creating the associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :post
-  has_many :comment, :through => :post
+  has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
